e3ccac1f18a278742dcd972c1e411e8d678c0ba8
Yanis Guenane <yguenane@gmail.com>
2012-09-11 15:04:53 +0200

    The yum-[plugin-]priorities package get installed with the correct name

---
+++ manifests/init.pp
@@ -0,0 +1,24 @@
+# Class: yum-priorities
+#
+#  This class installs yum-priorities package
+#
+# Actions:
+#  Install the yum-priorities package
+#
+# Requires:
+#  An OS that uses yum as it's primary package manager
+#
+# Sample Usage:
+#  include yum-priorities
+#
+class yum-priorities {
+
+    $yum_priorities = $lsbmajdistrelease ? {
+      /4|6/	=> 'yum-plugin-priorities',
+      '5'		=> 'yum-priorities',
+    }
+
+    package {$yum_priorities :
+      ensure    => installed,
+    }
+}
